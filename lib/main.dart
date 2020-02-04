e68a86f87c4c2fd2f9988805d278bb645dcf1f6c
Alessandro Falcetta <alessandro.falcetta@mail.polimi.it>
2020-02-04 12:11:01 +0100

    Improvements to profile page

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:AlGa/home_page.dart';
-import './register_page.dart';
 import './signin_page.dart';
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -22,7 +21,7 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.blue,
         ),
         home: FutureBuilder(
-          // get the Provider, and call the getUser method
+          // check if an user is already logged in
           future: _auth.currentUser(),
           // wait for the future to resolve and render the appropriate
           // widget for HomePage or LoginPage
@@ -37,61 +36,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-//class MyApp extends StatelessWidget {
-//  @override
-//  Widget build(BuildContext context) {
-//    return MaterialApp(
-//      title: 'Check User Screen',
-//      home: MyHomePage(title: 'AlGa'),
-//    );
-//  }
-//}
-//
-//class MyHomePage extends StatefulWidget {
-//  MyHomePage({Key key, this.title}) : super(key: key);
-//
-//  final String title;
-//
-//  @override
-//  _MyHomePageState createState() => _MyHomePageState();
-//}
-//
-//class _MyHomePageState extends State<MyHomePage> {
-//  FirebaseUser user;
-//  @override
-//  Widget build(BuildContext context) {
-//    return Scaffold(
-//      appBar: AppBar(
-//        title: Text(widget.title),
-//      ),
-//      body: Column(
-//        crossAxisAlignment: CrossAxisAlignment.start,
-//        children: <Widget>[
-//          Container(
-//            child: RaisedButton(
-//              child: const Text('Test registration'),
-//              onPressed: () => _pushPage(context, RegisterPage()),
-//            ),
-//            padding: const EdgeInsets.all(16),
-//            alignment: Alignment.center,
-//          ),
-//          Container(
-//            child: RaisedButton(
-//              child: const Text('Test SignIn/SignOut'),
-//              onPressed: () => _pushPage(context, SignInPage()),
-//            ),
-//            padding: const EdgeInsets.all(16),
-//            alignment: Alignment.center,
-//          ),
-//        ],
-//      ),
-//    );
-//  }
-//
-//  void _pushPage(BuildContext context, Widget page) {
-//    Navigator.of(context).push(
-//      MaterialPageRoute<void>(builder: (_) => page),
-//    );
-//  }
-//}
--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -1,4 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
+final StorageReference storageReference = FirebaseStorage().ref().child(
+    'gs://dima-project-alga.appspot.com/users_profilepics' + '/'
+);
+
+
+var _profileImageUrl;// = 'https://s14-eu5.startpage.com/cgi-bin/serveimage?url=https%3A%2F%2Fi.ytimg.com%2Fvi%2FXjkMMkfUQWA%2Fhqdefault.jpg&sp=3b1d4d4cbe35fbd0f12087a53ee33fbe&anticache=339340';
+var _name = "novalue";
+var _car = "novalue";
+
+Future <bool> getData() async {
+  var user = await _auth.currentUser();
+  var userUid = user.uid;
+  Firestore.instance
+      .collection("users")
+      .document(userUid)
+      .get()
+      .then((DocumentSnapshot ds) {
+        _name = ds["name"];
+        _car = ds["car"];
+  });
+  print("hello");
+  var storage = FirebaseStorage.instance.ref().child(
+      "users_profilepics/hx0i0SjHA1YFSFv2RODMiekSrae2"
+  );
+
+  _profileImageUrl = await storage.getDownloadURL();
+  print("hello2");
+//  return Future.delayed((Duration(seconds: 2)), () => true);
+}
+    
 
 class Profile extends StatelessWidget {
   @override
@@ -9,14 +44,27 @@ class Profile extends StatelessWidget {
           SizedBox(
             height: 30,
           ),
-          CircleAvatar(
-            radius: 80,
-            backgroundImage: AssetImage('assets/user_profile.jpg'),
-          ),
+          FutureBuilder<bool>(
+            future: getData(),
+            builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
+              if (!snapshot.hasData) {
+              // while data is loading:
+              return Center(
+              child: CircularProgressIndicator(),
+              );
+              } else {
+              return CircleAvatar(
+                  radius: 80,
+                  backgroundImage: NetworkImage(
+                    _profileImageUrl,
+                  )
+              );
+            }
+              }),
           Padding(
             padding: EdgeInsets.all(20.0),
               child: Text(
-                'Giulia Tamburini',
+                _name,
                 style: TextStyle(fontSize: 22),
               )
           ),
@@ -24,7 +72,9 @@ class Profile extends StatelessWidget {
             'Your selected Car',
             style: TextStyle(fontSize: 15),
           ),
-          Text('Tesla')
+          Text(
+            _car,
+          ),
         ],
       ),
     );
