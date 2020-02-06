66b7c7d44eeeaf8e068835ad1ab38fb1b198b212
Gabriele Guelfi <gabriele.guelfi@mail.polimi.it>
2020-02-06 17:53:52 +0100

    InfoWindow improved

--- lib/charging_stations.dart
+++ lib/charging_stations.dart
@@ -1,17 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 
 class ChargingStations{
-  var _pos;
-
-  Future<void> getData() async {
-    Firestore.instance
-        .collection("Charging_stations")
-        .document("eY12sQWkyATy9hnXiwtU")
-        .get()
-        .then((DocumentSnapshot ds) {
-      _pos = ds["pos"];
-    });
+  ChargingStations(GeoPoint pos, int speed, double price) {
+    this.pos = pos;
+    this.speed = speed;
+    this.price = price;
   }
+
+  GeoPoint pos;
+  int speed;
+  double price;
 }
--- lib/gmaps.dart
+++ lib/gmaps.dart
@@ -8,6 +8,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:AlGa/charging_stations.dart';
 
 class Gmaps extends StatefulWidget {
   @override
@@ -17,14 +18,20 @@ class Gmaps extends StatefulWidget {
 
 class _Gmaps extends State<Gmaps> {
   Completer<GoogleMapController> _mapController = Completer();
-  final Map<String, Marker> _markers = {};
-  BitmapDescriptor myIcon;
-  List<GeoPoint> _pos = new List();
+  Map<String, Marker> _markers = {};
+  BitmapDescriptor carIcon;
+  BitmapDescriptor stationIcon;
+  List<ChargingStations> stations = new List();
+
+  double pinPillPosition = -100;
+
+  ChargingStations currentlySelectedStation = new ChargingStations(new GeoPoint(0, 0), 0, 0);
 
   final LatLng _center = const LatLng(45.4642, 9.1900);
 
   void _onMapCreated(GoogleMapController controller) {
     _mapController.complete(controller);
+    _getLocation();
   }
 
   @override
@@ -34,9 +41,30 @@ class _Gmaps extends State<Gmaps> {
   }
 
   void setCustomMapPin() async {
-    final Uint8List markerIcon = await getBytesFromAsset('assets/car_icon.png', 150);
-    myIcon = BitmapDescriptor.fromBytes(markerIcon);
+    final Uint8List car = await getBytesFromAsset('assets/car_icon.png', 150);
+    final Uint8List station = await getBytesFromAsset('assets/station_green.png', 140);
+    carIcon = BitmapDescriptor.fromBytes(car);
+    stationIcon = BitmapDescriptor.fromBytes(station);
+
     await getData();
+
+    setState(() {
+      int i = 0;
+      for (var elem in stations) {
+        _markers["${i}"] = Marker(
+          markerId: MarkerId("charger: ${i}"),
+          position: LatLng(elem.pos.latitude, elem.pos.longitude),
+          onTap: () {
+            setState(() {
+              currentlySelectedStation = elem;
+              pinPillPosition = 0;
+            });
+          },
+          icon: stationIcon,
+        );
+        i++;
+      }
+    });
   }
 
   Future<Uint8List> getBytesFromAsset(String path, int width) async {
@@ -47,38 +75,95 @@ class _Gmaps extends State<Gmaps> {
   }
 
   Future<void> getData() async {
-
     final QuerySnapshot result = await Firestore.instance.collection('charging_stations').getDocuments();
     final List<DocumentSnapshot> documents = result.documents;
 
-    documents.forEach((data) => _pos.add(data["pos"]));
-    /*
-    Firestore.instance
-        .collection("charging_stations")
-        .document("eY12sQWkyATy9hnXiwtU")
-        .get()
-        .then((DocumentSnapshot ds) {
-      _pos = ds["pos"];
-    });
-    */
+    documents.forEach((data) => stations.add(ChargingStations(data["pos"], data["speed"], data["price"])));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-        onMapCreated: _onMapCreated,
-        initialCameraPosition: CameraPosition(
-          target: _center,
-          zoom: 13.0,
-        ),
-        markers: _markers.values.toSet(),
-      ),
+      body: Stack(
+        children: <Widget> [
+          GoogleMap(
+            onMapCreated: _onMapCreated,
+            initialCameraPosition: CameraPosition(
+              target: _center,
+              zoom: 13.0,
+            ),
+            markers: _markers.values.toSet(),
+            onTap: (LatLng location) {
+              setState(() {
+                pinPillPosition = -100;
+              });
+            },
+          ),
+          AnimatedPositioned(
+            bottom: pinPillPosition, right: 0, left: 0,
+            duration: Duration(milliseconds: 200),
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                margin: EdgeInsets.all(20),
+                height: 70,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(50)),
+                  boxShadow: <BoxShadow>[
+                    BoxShadow(
+                      blurRadius: 20,
+                      offset: Offset.zero,
+                      color: Colors.grey.withOpacity(0.5)
+                    )
+                  ]
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Expanded(
+                      child: Container(
+                        margin: EdgeInsets.only(left: 20),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              "Price: ${currentlySelectedStation.price.toString()}€/kW",
+                              style: TextStyle(
+                                color: Colors.green
+                              )
+                            ),
+                            Text(
+                              "Speed: ${currentlySelectedStation.speed.toString()}kW/h",
+                              style: TextStyle(
+                                fontSize: 12,
+                                color: Colors.grey
+                              )
+                            ),
+                            Text("Available: YES",
+                              style: TextStyle(
+                                fontSize: 12,
+                                color: Colors.grey
+                              )
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ]
+                )
+              )
+            )
+          )
+        ],
+    ),
       floatingActionButton: FloatingActionButton(
         onPressed: _getLocation,
         tooltip: 'Get Location',
-        child: Icon(Icons.flag),
-      ),
+        child: Icon(Icons.gps_fixed),
+      )
     );
   }
 
@@ -88,23 +173,13 @@ class _Gmaps extends State<Gmaps> {
     await _moveToPosition(currentLocation);
 
     setState(() {
-      _markers.clear();
       final marker = Marker(
         markerId: MarkerId("curr_loc"),
         position: LatLng(currentLocation.latitude, currentLocation.longitude),
         infoWindow: InfoWindow(title: 'Your Location'),
-        icon: myIcon
+        icon: carIcon
       );
-      _markers["Current Location"] = marker;
-      int i = 0;
-      for (var elem in _pos) {
-        _markers["${i}"] = Marker(
-          markerId: MarkerId("charger: ${i}"),
-          position: LatLng(elem.latitude, elem.longitude),
-          infoWindow: InfoWindow(title: "Charger: ${i}"),
-        );
-        i++;
-      }
+      _markers.update("Current Location", (existingValue) => marker, ifAbsent: () => marker,);
     });
   }
 
